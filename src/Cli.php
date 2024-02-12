e525f304ccc4d875cdf8f28e36eac687090413d0
[Akorsikov] <[akors@mail.ru]>
2024-02-12 15:45:00 +0300

    refactor: type bool for greeting()

--- src/Cli.php
+++ src/Cli.php
@@ -5,10 +5,10 @@ namespace Php\Project\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greeting(): null
+function greeting(): bool
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    return null;
+    return true;
 }
