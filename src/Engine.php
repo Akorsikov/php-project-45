d456096fc08ce6efca1bb33d38b4373275a1319b
[Akorsikov] <[akors@mail.ru]>
2024-02-21 11:55:21 +0300

    feat: realise rounds cycle into Engine; mentor comment #6,8

--- src/Engine.php
+++ src/Engine.php
@@ -5,13 +5,20 @@ namespace Php\Project\Engine;
 use function cli\line;
 use function cli\prompt;
 
+const NUMBER_OF_ROUNDS = 3;
+
 function engine(callable $game): void
 {
     line('', 'Welcome to the Brain Games!');
     $gamerName = prompt('May I have your name?');
     line('Hello, %s', $gamerName);
 
-    $winner = call_user_func($game);
+    $roundsCounter = 1;
+
+    do {
+        $winner = call_user_func($game);
+        $roundsCounter++;
+    } while ($winner && $roundsCounter <= NUMBER_OF_ROUNDS);
 
     if ($winner) {
         line("Congratulations, %s!", $gamerName);
