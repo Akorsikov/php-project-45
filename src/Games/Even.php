31bc28ad7280c9cab20a13aeddb74b5f2e809781
[Akorsikov] <[akors@mail.ru]>
2024-02-21 12:56:49 +0300

    fix: delete cycle from Even.php & fix according mentor comment #10

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -5,41 +5,36 @@ namespace Php\Project\Even;
 use function cli\line;
 use function cli\prompt;
 
-const CORRECT_ANSWER = 3;
 const START_RANDOM_NUMBER = 0;
 const FINISH_RANDOM_NUMBER = 100;
 
 function checkParityGame(): bool
 {
     line('Answer "yes" if the number is even, otherwise answer "no".');
-    $countCorrectAnswer = 0;
 
-    do {
-        $randomNumber = rand(START_RANDOM_NUMBER, FINISH_RANDOM_NUMBER);
-        $evenNumber = ($randomNumber % 2 === 0 ) ? true : false;
-        line("Question: %s", $randomNumber);
-        $gamerAnswer = prompt('Your answer');
+    $randomNumber = rand(START_RANDOM_NUMBER, FINISH_RANDOM_NUMBER);
+    $evenNumber = ($randomNumber % 2 === 0);
+    line("Question: %s", $randomNumber);
+    $gamerAnswer = prompt('Your answer');
 
-        if (
-            $gamerAnswer === 'yes' && $evenNumber
-            || $gamerAnswer === 'no' && !$evenNumber
-        ) {
-            $countCorrectAnswer++;
-            line('Correct!');
+    if (
+        $gamerAnswer === 'yes' && $evenNumber
+        || $gamerAnswer === 'no' && !$evenNumber
+    ) {
+        line('Correct!');
+
+        return true;
+    } else {
+        if ($evenNumber) {
+            $correctAnswer = ($gamerAnswer === 'yes') ? 'no' : 'yes';
         } else {
-            if ($evenNumber) {
-                $correctAnswer = ($gamerAnswer === 'yes') ? 'no' : 'yes';
-            } else {
-                $correctAnswer = ($gamerAnswer === 'no') ? 'yes' : 'no';
-            }
-            line(
-                '\'%s\' is wrong answer ;(. Correct answer was \'%s\'.',
-                $gamerAnswer,
-                $correctAnswer
-            );
-            break;
+            $correctAnswer = ($gamerAnswer === 'no') ? 'yes' : 'no';
         }
-    } while ($countCorrectAnswer < CORRECT_ANSWER);
-
-    return $countCorrectAnswer === CORRECT_ANSWER;
+        line(
+            '\'%s\' is wrong answer ;(. Correct answer was \'%s\'.',
+            $gamerAnswer,
+            $correctAnswer
+        );
+        return false;
+    }
 }
