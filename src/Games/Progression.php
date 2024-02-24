2d2b0e1fd22e339683411b42eacbd3d0901b8c08
[Akorsikov] <[akors@mail.ru]>
2024-02-24 12:11:25 +0300

    refactor: remove cycle

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -5,7 +5,6 @@ namespace Php\Project\Progression;
 use function cli\line;
 use function cli\prompt;
 
-const CORRECT_ANSWER = 3;
 const MIN_LENGTH_PROGRESSION = 5;
 const MAX_LENGTH_PROGRESSION = 10;
 const MIN_START_ITEM = 0;
@@ -17,29 +16,29 @@ function checkProgressionGame(): bool
 {
     line('What number is missing in the progression?');
 
-    $countCorrectAnswer = 0;
+    $lengthProgression = rand(MIN_LENGTH_PROGRESSION, MAX_LENGTH_PROGRESSION);
+    $stepProgression = rand(MIN_STEP_PROGRESSION, MAX_STEP_PROGRESSION);
+    $startItem = rand(MIN_START_ITEM, MAX_START_ITEM);
+    $endItem = $startItem + ($lengthProgression - 1) * $stepProgression;
+    $progression = range($startItem, $endItem, $stepProgression);
 
-    do {
-        $lengthProgression = rand(MIN_LENGTH_PROGRESSION, MAX_LENGTH_PROGRESSION);
-        $stepProgression = rand(MIN_STEP_PROGRESSION, MAX_STEP_PROGRESSION);
-        $startItem = rand(MIN_START_ITEM, MAX_START_ITEM);
-        $endItem = $startItem + ($lengthProgression - 1) * $stepProgression;
-        $progression = range($startItem, $endItem, $stepProgression);
-        $indexRequireNumber = rand(0, $lengthProgression - 1);
-        $correctAnswer = array_splice($progression, $indexRequireNumber, 1, ['..']);
-        $correctAnswer = join('', $correctAnswer);
-        $stringQuestion = join(' ', $progression);
-        line("Question: %s", $stringQuestion);
-        $gamerAnswer = prompt('Your answer');
-        if ($gamerAnswer === $correctAnswer) {
-            $countCorrectAnswer++;
-            line('Correct!');
-        } else {
-            line('\'%s\' is wrong answer ;(. Correct answer was \'%s\'.', $gamerAnswer, $correctAnswer);
+    $indexRequireNumber = rand(0, $lengthProgression - 1);
+    $correctAnswer = (string) array_splice(
+        $progression,
+        $indexRequireNumber,
+        1,
+        ['..']
+    )[0];
+    $stringQuestion = join(' ', $progression);
 
-            break;
-        }
-    } while ($countCorrectAnswer < CORRECT_ANSWER);
+    line("Question: %s", $stringQuestion);
+    $gamerAnswer = prompt('Your answer');
 
-    return $countCorrectAnswer === CORRECT_ANSWER;
+    if ($gamerAnswer === $correctAnswer) {
+        line('Correct!');
+        return true;
+    } else {
+        line('\'%s\' is wrong answer ;(. Correct answer was \'%s\'.', $gamerAnswer, $correctAnswer);
+        return false;
+    }
 }
