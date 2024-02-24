2c3a4c861e85ac63ac9e6c80cbaa002c765113be
[Akorsikov] <[akors@mail.ru]>
2024-02-24 11:21:49 +0300

    refactor: remove cycle & exchange array on function

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -5,36 +5,31 @@ namespace Php\Project\Prime;
 use function cli\line;
 use function cli\prompt;
 
-const CORRECT_ANSWER = 3;
-const START_RANDOM_NUMBER = 0;
+const START_RANDOM_NUMBER = 1;
 const FINISH_RANDOM_NUMBER = 100;
-const PRIME_NUMBERS =
-    [
-        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
-        43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
-    ];
+
+function isSimpleNumber(int $number): bool
+{
+    $halfNumber = floor($number / 2);
+    for ($i = $halfNumber; $i > 1; $i--) {
+        if ($number % $i === 0 && $i > 1) {
+            return false;
+        }
+    }
+    return true;
+}
 
 function checkPrimeGame(): bool
 {
     line('Answer "yes" if given number is prime. Otherwise answer "no".');
-
-    $countCorrectAnswer = 0;
-
-    do {
-        $randomNumber = rand(START_RANDOM_NUMBER, FINISH_RANDOM_NUMBER);
-        $isPrimeNumber = in_array($randomNumber, PRIME_NUMBERS, true);
-        $correctAnswer = $isPrimeNumber ? 'yes' : 'no';
-        line("Question: %s", $randomNumber);
-        $gamerAnswer = prompt('Your answer');
-        if ($isPrimeNumber && $gamerAnswer === $correctAnswer || !$isPrimeNumber && $gamerAnswer === $correctAnswer) {
-            $countCorrectAnswer++;
-            line('Correct!');
-        } else {
-            line('\'%s\' is wrong answer ;(. Correct answer was \'%s\'.', $gamerAnswer, $correctAnswer);
-
-            break;
-        }
-    } while ($countCorrectAnswer < CORRECT_ANSWER);
-
-    return $countCorrectAnswer === CORRECT_ANSWER;
+    $randomNumber = rand(START_RANDOM_NUMBER, FINISH_RANDOM_NUMBER);
+    $correctAnswer = isSimpleNumber($randomNumber) ? 'yes' : 'no';
+    line("Question: %s", $randomNumber);
+    $gamerAnswer = prompt('Your answer');
+    if ($gamerAnswer === $correctAnswer) {
+        return true;
+    } else {
+        line('\'%s\' is wrong answer ;(. Correct answer was \'%s\'.', $gamerAnswer, $correctAnswer);
+        return false;
+    }
 }
