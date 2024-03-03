c57c8295d76f2a43ca43d4d917adb4e0cf1e2187
[Akorsikov] <[akors@mail.ru]>
2024-03-03 15:15:22 +0300

    fix: remove all output into console, according mentor comment #6

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -2,40 +2,19 @@
 
 namespace Php\Project\Even;
 
-use function cli\line;
-use function cli\prompt;
-
 const START_RANDOM_NUMBER = 0;
 const FINISH_RANDOM_NUMBER = 100;
+const CONDITION = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-function checkParityGame(): bool
+/**
+ * @return array<string>
+ */
+function checkParityGame(): array
 {
-    line('Answer "yes" if the number is even, otherwise answer "no".');
-
     $randomNumber = rand(START_RANDOM_NUMBER, FINISH_RANDOM_NUMBER);
     $isEvenNumber = ($randomNumber % 2 === 0);
-    line("Question: %s", $randomNumber);
-    $gamerAnswer = prompt('Your answer');
+    $task = "Question: $randomNumber";
+    $correctAnswer = ($isEvenNumber) ? 'yes' : 'no';
 
-    if (
-        $gamerAnswer === 'yes' && $isEvenNumber
-        || $gamerAnswer === 'no' && !$isEvenNumber
-    ) {
-        line('Correct!');
-
-        return true;
-    } else {
-        if ($isEvenNumber) {
-            $correctAnswer = ($gamerAnswer === 'yes') ? 'no' : 'yes';
-        } else {
-            $correctAnswer = ($gamerAnswer === 'no') ? 'yes' : 'no';
-        }
-        line(
-            '\'%s\' is wrong answer ;(. Correct answer was \'%s\'.',
-            $gamerAnswer,
-            $correctAnswer
-        );
-
-        return false;
-    }
+    return array(CONDITION, $task, $correctAnswer);
 }
