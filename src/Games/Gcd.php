6fbe0fd24af29d74ca8d41e0ddcda9a9ef48e1f7
[Akorsikov] <[akors@mail.ru]>
2024-03-03 15:59:15 +0300

    fix: remove unnecessary code

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -2,40 +2,17 @@
 
 namespace Php\Project\Gcd;
 
-const START_RANDOM_NUMBER = 4;
+const START_RANDOM_NUMBER = 0;
 const FINISH_RANDOM_NUMBER = 100;
-const LOOPS_NUMBER = 10;
 const CONDITION = 'Find the greatest common divisor of given numbers.';
-
-function isSimpleNumber(int $number): bool
-{
-    $halfNumber = floor($number / 2);
-    for ($i = $halfNumber; $i > 1; $i--) {
-        if ($number % $i === 0 && $i > 1) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function getNotSimleRand(): int
-{
-    $loopsCounter = 1;
-    do {
-        $randomNumber = rand(START_RANDOM_NUMBER, FINISH_RANDOM_NUMBER);
-        $loopsCounter++;
-    } while (isSimpleNumber($randomNumber) && $loopsCounter <= LOOPS_NUMBER);
-    return $randomNumber;
-}
 
 /**
  * @return array<string>
  */
 function checkGcdGame(): array
 {
-
-    $randomNumberOne = getNotSimleRand();
-    $randomNumberTwo = getNotSimleRand();
+    $randomNumberOne = rand(START_RANDOM_NUMBER, FINISH_RANDOM_NUMBER);
+    $randomNumberTwo = rand(START_RANDOM_NUMBER, FINISH_RANDOM_NUMBER);
     $firstDivisor = ($randomNumberOne < $randomNumberTwo) ? $randomNumberOne : $randomNumberTwo;
     $correctAnswer = '1';
 
