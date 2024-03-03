bff6c8811a9e95e70188a9f00ac05736f20c54cb
[Akorsikov] <[akors@mail.ru]>
2024-03-03 16:42:58 +0300

    refactor: rename function isSimpleNumber to isPrimeNumber

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -6,7 +6,7 @@ const START_RANDOM_NUMBER = 1;
 const FINISH_RANDOM_NUMBER = 100;
 const CONDITION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-function isSimpleNumber(int $number): bool
+function isPrimeNumber(int $number): bool
 {
     $halfNumber = floor($number / 2);
     for ($i = $halfNumber; $i > 1; $i--) {
@@ -23,7 +23,7 @@ function isSimpleNumber(int $number): bool
 function checkPrimeGame(): array
 {
     $randomNumber = rand(START_RANDOM_NUMBER, FINISH_RANDOM_NUMBER);
-    $correctAnswer = isSimpleNumber($randomNumber) ? 'yes' : 'no';
+    $correctAnswer = isPrimeNumber($randomNumber) ? 'yes' : 'no';
     $task = "Question: $randomNumber";
 
     return array(CONDITION, $task, $correctAnswer);
