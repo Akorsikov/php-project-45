497157d696ca2027348e18919f3dfabb569e415d
[Akorsikov] <[akors@mail.ru]>
2024-03-03 11:29:07 +0300

    fix: remove boolean type from greeting(), according to the mentor comment #3

--- src/Cli.php
+++ src/Cli.php
@@ -5,10 +5,9 @@ namespace Php\Project\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greeting(): bool
+function greeting(): void
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    return true;
 }
